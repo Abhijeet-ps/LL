b63e9aca90e4fc359f2633d11218bb36448d16aa
Abhijeet PS <73816451+Abhijeet-ps@users.noreply.github.com>
2023-07-14 18:31:36 +0530

    Updated the middleware/index.ts to eliminate the jwt expired error.

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,9 +1,9 @@
-import jwt from "jsonwebtoken"
-import { NextFunction, Request, Response } from "express"
-import User from "../models/user-model"
+import jwt, { TokenExpiredError } from "jsonwebtoken";
+import { NextFunction, Request, Response } from "express";
+import User from "../models/user-model";
 
 export interface AuthRequest extends Request {
-  user: string
+  user: string;
 }
 
 export const authenticationMiddleware = async (
@@ -12,22 +12,94 @@ export const authenticationMiddleware = async (
   next: NextFunction
 ) => {
   try {
-    const { authorization } = request.headers
+    const { authorization } = request.headers;
     if (!authorization) {
       return response.status(401).json({
         error: "Authorization is required",
-      })
+      });
     }
-    const token = authorization
-    const { _id } = jwt.verify(token, "express")
-    const existingUser = await User.findOne({ _id })
+    const token = authorization;
+    let decodedToken;
+    try {
+      decodedToken = jwt.verify(token, "express");
+    } catch (error) {
+      if (error instanceof TokenExpiredError) {
+        try {
+          // Generate a new token with the same payload as the existing token
+          const newToken = generateNewToken(decodedToken);
+          decodedToken = jwt.verify(newToken, "express"); // Verify the new token
+          response.setHeader("Authorization", newToken); // Update the response headers with the new token
+        } catch (err) {
+          console.log("Error generating new token:", err);
+          return response.status(401).json({
+            error: "Invalid token",
+          });
+        }
+      } else {
+        throw error;
+      }
+    }
+
+    const { _id } = decodedToken;
+    const existingUser = await User.findOne({ _id });
 
     if (existingUser) {
-      request.user = existingUser.id
+      request.user = existingUser.id;
     }
-    next()
+    next();
   } catch (error) {
-    console.log("error in authenticationMiddleware", error)
-    throw error
+    console.log("error in authenticationMiddleware", error);
+    throw error;
   }
+};
+
+function generateNewToken(decodedToken: any): string {
+  const payload = decodedToken; // Use the same payload from the existing token
+  const newToken = jwt.sign(payload, "express", {
+    expiresIn: "30d", // Set the expiration for the new token
+  });
+  return newToken;
 }
+
+
+
+
+
+
+
+
+
+
+// import jwt from "jsonwebtoken"
+// import { NextFunction, Request, Response } from "express"
+// import User from "../models/user-model"
+
+// export interface AuthRequest extends Request {
+//   user: string
+// }
+
+// export const authenticationMiddleware = async (
+//   request: AuthRequest,
+//   response: Response,
+//   next: NextFunction
+// ) => {
+//   try {
+//     const { authorization } = request.headers
+//     if (!authorization) {
+//       return response.status(401).json({
+//         error: "Authorization is required",
+//       })
+//     }
+//     const token = authorization
+//     const { _id } = jwt.verify(token, "express")
+//     const existingUser = await User.findOne({ _id })
+
+//     if (existingUser) {
+//       request.user = existingUser.id
+//     }
+//     next()
+//   } catch (error) {
+//     console.log("error in authenticationMiddleware", error)
+//     throw error
+//   }
+// }
